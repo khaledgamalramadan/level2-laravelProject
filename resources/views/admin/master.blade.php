bb042bce7c2c70f23b92f3bf87cd46950405d444
khaledgamalramadan <bebo50016@gmail.com>
2026-03-29 23:37:27 +0200

    Initializing invironment and integrate themes and Authentication

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+@include('admin.partials.head')
+
+<body class="vertical  light  ">
+    <div class="wrapper">
+        @include('admin.partials.navbar')
+
+        @include('admin.partials.sidpar')
+
+        <main role="main" class="main-content">
+
+            @yield('content')
+            
+        </main> <!-- main -->
+
+    </div> <!-- .wrapper -->
+
+    @include('admin.partials.scripts')
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+// use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::name('front.')->group(function () {
+    Route::view('/', 'front.index')->name('index');
+    Route::view('/about', 'front.about')->name('about');
+    Route::view('/service', 'front.service')->name('service');
+    Route::view('/contact', 'front.contact')->name('contact');
+});
+
+
+Route::name('admin.')->prefix('admin')->group(function () {
+    Route::middleware('auth')->group(function () {
+        Route::view('/', 'admin.index')->name('index');
+
+    });
+    require __DIR__.'/auth.php';
+});
+
+
+
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
